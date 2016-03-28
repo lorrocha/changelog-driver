cf502432aaba1a3effc1c3099af86cf163a762d7
Lorrayne Rocha <lrocha@rbmtechnologies.com>
2016-03-27 20:44:31 -0400

    Remove parsing logic from the driver.

--- lib/changelog_driver.rb
+++ lib/changelog_driver.rb
@@ -3,50 +3,7 @@ require 'changelog_parser'
 class ChangelogDriver
   attr_accessor :ancestor, :current, :other
 
-  # def initialize(ancestor, current, other)
-  #   # ancestor = File.readlines ancestor
-  #   # @file_header = parse_header ancestor
-  #   #
-  #   # @ancestor = parse_changelog ancestor.drop(@file_header.count)
-  #   # # @current = File.readlines current
-  #   # # @other = File.readlines other
-  # end
-
-  def parse_header(file)
-    header = []
-
-    file.each do |line|
-      if line =~ /^[^#]/ || line[0,3].count('#') == 1
-        header << line
-      else
-        return header
-      end
-    end
+  def initialize(ancestor, current, other)
+    parser = ChangelogParser.new
   end
-
-  def parse_changelog(lines)
-    descriptions = []
-    sub_container = {}
-    container = {}
-
-    lines.reverse.each do |line|
-      if line =~ /^[^#]/
-        descriptions.unshift line
-      else
-        if hashes = line[0,3].count('#') == 3
-          sub_container[line] = descriptions
-
-          descriptions = []
-        else
-          container[line] = sub_container
-
-          sub_container = {}
-          descriptions = []
-        end
-      end
-    end
-
-    container
-  end
-
 end
