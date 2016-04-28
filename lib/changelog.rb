0a06bf6b7954f6601f3727ce806fc5dcafbdfb3e
Lorrayne Rocha <lrocha@rbmtechnologies.com>
2016-04-28 16:39:58 -0400

    introduce way to sort releases

--- lib/changelog.rb
+++ lib/changelog.rb
@@ -4,4 +4,19 @@ class Changelog
   def initialize
     @releases = []
   end
+
+  def unreleased
+    releases.detect { |r| /[Uu]nreleased/ =~ r.title }
+  end
+
+  def sorted_releases
+    released = releases - [unreleased]
+
+    [unreleased] | released.sort_by { |r|
+      /\A\[(?<version>.+)\] - (?<date>.+)/ =~ r.title
+
+      # sort by version, but in worst case scenario, include date sorting
+      version.split('.').map(&:to_i) | [date.split('-')]
+    }.reverse
+  end
 end
